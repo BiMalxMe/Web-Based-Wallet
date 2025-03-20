ca5ffb4e72074c219246dd7bc1c13cda31521fdb
BiMalxMe <journeyxbimal@gmail.com>
2025-03-20 07:21:29 +0545

    ItisRenderedOnMainPageWhenBtnAreNotSelected

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -10,7 +10,7 @@ export const Main = ({solana,etherium}: Props ) => {
     return (
         <div className="flex flex-col items-center pt-40">
       <div className="text-3xl font-extrabold text-slate-400 mb-5">KhaTa Supports Multiple Blockchain</div>
-      <div className="text-xl  text-slate-300 font-semibold">Choose One of the Given two</div>
+      <div className="text-xl  text-slate-300 font-extrabold">Choose One of the Given two</div>
       <div className="flex w-2/4 justify-center items-center gap-4 mt-6">
         <Button text="Solana" onClick={solana}/>
         <Button text="Ethereum" onClick={etherium} />
