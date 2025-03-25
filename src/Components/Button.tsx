303a8430ff1b717a76070c428834ba75191161b6
BiMalxMe <journeyxbimal@gmail.com>
2025-03-25 09:30:50 +0545

    ModifyingTheBtnComponent

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -9,7 +9,7 @@ export const Button= ({text,onClick,color} : ButtonProps) => {
     return (
         <button
         onClick={onClick}
-        className={`text-xl active:scale-70 cursor-pointer px-6 py-4 ${color == null ? "bg-white" : "bg-" + color + "-700"} rounded-3xl text-black hover:bg-slate-200`}>
+        className={`text-xl active:scale-70 cursor-pointer px-6 py-4  bg-slate-900  text-white hover:bg-gray-900 active:scale-75 rounded-3xl`}>
         {text}
       </button>
     )
