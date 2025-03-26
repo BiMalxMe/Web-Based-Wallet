ffc6c34296e223e39af4de69affc072dfd94629a
BiMalxMe <journeyxbimal@gmail.com>
2025-03-26 08:02:22 +0545

    AddingTheClickStateEnumToDistinguishGeneratedAndNonGenerated

--- src/Components/AddDelWallet.tsx
+++ src/Components/AddDelWallet.tsx
@@ -7,10 +7,14 @@ enum stateProp {
   hide,
   add,
 }
+enum clickProp {
+  yes,
+  no
+}
 
 export const AddDelWallet = () => {
   const [status, setStatus] = useState<stateProp | undefined>(stateProp.show); // Wallet add ya delete garne state track garxa
-
+  const [clicked,setClicked] = useState<clickProp >(clickProp.no)
   return (
     <div className="min-h-screen">
       {/* Upar ko section jaha button haru cha */}
@@ -23,6 +27,7 @@ export const AddDelWallet = () => {
               onClick={() => {
                 setStatus(stateProp.show);
                 setStatus(stateProp.hide);
+                setClicked(clickProp.yes)
                 setTimeout(() => {
                   setStatus(stateProp.add);
                 },100);
@@ -39,7 +44,9 @@ export const AddDelWallet = () => {
       {/* Blockchain component display garne thau */}
       <div className="flex justify-center items-center mt-4">
         {/* Jaba status "add" huncha taba matra Blockchain component render huncha */}
-        {status === stateProp.add && <Blockchain type="solana" />}
+        {status === stateProp.add && (
+  <Blockchain type="solana" generated={clicked === clickProp.yes ? "yes" : "no"} />
+)}
       </div>
     </div>
   );
