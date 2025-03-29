738155f84da6c8e93a3b392d1b1e0b9130ed15be
BiMalxMe <journeyxbimal@gmail.com>
2025-03-29 08:04:43 +0545

    AddedGradientColoringInTheBtnComp

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,16 +1,17 @@
-
-interface ButtonProps{
+interface ButtonProps {
     onClick?: () => void;
-    text :string;
-    color? : string
-}
-
-export const Button= ({text,onClick} : ButtonProps) => {
+    text: string;
+    color?: string;
+  }
+  
+  export const Button = ({ text, onClick }: ButtonProps) => {
     return (
-        <button
+      <button
         onClick={onClick}
-        className={`text-xl  cursor-pointer px-6 py-4  bg-slate-900  text-white hover:bg-gray-900 active:scale-75 rounded-3xl`}>
+        className="text-xl cursor-pointer px-6 py-4 bg-slate-900 text-white hover:bg-gray-800 active:scale-90 
+                   rounded-3xl transition-all duration-300 shadow-lg hover:shadow-xl hover:opacity-90">
         {text}
       </button>
-    )
-}
+    );
+  };
+  
